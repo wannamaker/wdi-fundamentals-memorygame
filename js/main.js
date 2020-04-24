766cf09908fdf50c8b9adae90477b6e216743b87
Wannamaker <wannamaker.charles@outlook.com>
2020-04-24 06:05:06 -0400

    created game logic -- functions

--- js/main.js
+++ js/main.js
@@ -15,26 +15,79 @@ let cardFour = "king";
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
+/*
 
-let twoCardsPlay = false;
 
-cardsInPlay.push(cardOne);
+*/
 
-console.log("User flipped queen");
+function checkForMatch() {
 
-let cardTwo = cards[2]
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2){
-	twoCardsPlay = true;
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match");
 	} else {
 		alert("Sorry, try again");
 	}
+};
+
+
+function flipCard(cardId) {
+
+//let cardOne = cards[0];
+
+
+
+//cardsInPlay.push(cardOne);
+
+//console.log("User flipped queen");
+
+//let cardTwo = cards[2]
+
+//cardsInPlay.push(cardOne);
+
+//console.log("User flipped king");
+
+//console.log("user flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);
+
+checkForMatch();
+
+/*
+let twoCardsPlay = false;
+if (cardsInPlay.length === 2){
+	twoCardsPlay = true;
+
+	
 }
+*/
+
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
